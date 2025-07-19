5862dae8e7861642c07f39ffdf5fe6225469e0cc
BrutalRemains <kristianscottroberts@gmail.com>
2025-07-19 11:51:27 -0400

    added the player into the main function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
-    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     run = True
@@ -14,9 +15,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        SCREEN.fill("black")
+        screen.fill("black")
+        
+        player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        player.draw(screen)
+        
         pygame.display.flip()
-
         dt += clock.tick(60) / 1000
     
     pygame.quit()
