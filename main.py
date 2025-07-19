0fac472e2823c6ec1d9e23381ebbfd0547f871a4
BrutalRemains <kristianscottroberts@gmail.com>
2025-07-19 11:00:15 -0400

    Created black game window

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
+from constants import *
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
-    print("Starting Asteroids!")
+    run = True
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        SCREEN.fill((0,0,0))
+        pygame.display.flip()
+    
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
+
