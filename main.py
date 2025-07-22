28d3ba8cbe00e9ecc52d31bdb8bf1e7663f824cd
BrutalRemains <kristianscottroberts@gmail.com>
2025-07-21 20:36:47 -0400

    added asteroids! also adjusted player speed.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 
 def main():
@@ -8,22 +9,35 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     run = True
     
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
+    
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+        updatable.update(dt)
+        
         screen.fill("black")
         
-        
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()
-        dt += clock.tick(60) / 1000
+        
+        dt = clock.tick(60) / 1000
     
     pygame.quit()
 
